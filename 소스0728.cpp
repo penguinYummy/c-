aebb1293ac00665eb016287e58d25cbf3124c893
penguinYummy <a01062712541@gmail.com>
2025-07-30 16:51:56 +0900

    Add files via upload

--- 소스0728.cpp
+++ 소스0728.cpp
@@ -16,3 +16,82 @@
 //	printf("안녕하세요\n안녕하세요\n안녕허세요\n");
 //	return 0;
 //}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("사과 1개\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int apple = 1;
+//	printf("사과 %d\n", apple);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	printf("키보드 3개\n");
+//	printf("마우스 2개\n");
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int keyboard = 3;
+//	int mouse = 2;
+//	printf("키보드: %d개\n", keyboard);
+//	printf("마우스: %d개\n", mouse);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	int coin1 = 500;
+//	int coin2 = 1000;
+//	printf("%d + %d = %d\n",
+//		coin1, coin2, coin1 + coin2);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	char A;
+//	A = 'A';
+//	printf("A = %c, A = %d\n", A, A);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	double pi;
+//	pi = 3.14;
+//	float e = 2.718;
+//	printf("pi = %.2lf, e = %.3lf.. \n", pi, e);
+//	return 0;
+//}
+//#include <stdio.h>
+//int main()
+//{
+//	double pi;
+//	pi = 3.14;
+//	int r = 5;
+//	double Area;
+//	Area = pi * r * r;
+//	double round;
+//	round = 2 * pi * r;
+//	printf("원의 넓이는 %.1f\n", Area);
+//	printf("원의 둘레는 %.1f\n", round);
+//	return 0;
+//}
+#include <stdio.h>
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int h = 7;
+	int Area = (a + b) * h / 2;
+	printf("사다리꼴의 넓이는 %d", Area);
+	return 0;
+}
